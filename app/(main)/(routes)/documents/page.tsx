afce67352a08310cac03e7a60a5a584e2bbbc231
Jun Low <zhijun.low@gmail.com>
2023-10-28 15:32:55 +1100

    add document page and search function

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+
+const DocumentsPage = () => {
+  const router = useRouter();
+  const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" })
+      .then((documentId) => router.push(`/documents/${documentId}`))
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note has been successfully created.",
+      error: "Failed to create a new note. Please try again."
+    });
+  };
+
+  return ( 
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.svg"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+        priority={true}
+      />
+      <Image
+        src="/empty-dark.svg"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+        priority={true}
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Notionize
+      </h2>
+      <Button onClick={onCreate}>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+   );
+}
+ 
+export default DocumentsPage;
---
+++ components/modals/confirm-modal.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from "@/components/ui/alert-dialog";
+
+interface ConfirmModalProps {
+  children: React.ReactNode;
+  onConfirm: () => void;
+};
+
+export const ConfirmModal = ({
+  children,
+  onConfirm
+}: ConfirmModalProps) => {
+  const handleConfirm = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+    onConfirm();
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger onClick={(e) => e.stopPropagation()} asChild>
+        {children}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Delete
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Are you sure you want to delete this page permanently? This action cannot be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={e => e.stopPropagation()}>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction className="bg-red-100 hover:bg-red-100 text-red-600 border-red-500" onClick={handleConfirm}>
+            Confirm
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
