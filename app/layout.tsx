770e86e7cd37339cf5b3b830d93a3dc62c53dfa2
Jun Low <zhijun.low@gmail.com>
2023-10-28 15:47:10 +1100

    add settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ThemeProvider } from '@/components/providers/theme-provider'
-import { ConvexClientProvider } from '@/components/providers/convex-provider'
-import { Toaster } from 'sonner'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { Toaster } from "sonner";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Notionize',
-  description: 'Notionize is the connected workspace where better, faster work happens.',
+  title: "Notionize",
+  description:
+    "Notionize is the connected workspace where better, faster work happens.",
   icons: {
     icon: [
       {
@@ -24,12 +26,12 @@ export const metadata: Metadata = {
       },
     ],
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -41,11 +43,12 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-          <Toaster position="bottom-center" />
-          {children}
+            <Toaster position="bottom-center" />
+            <ModalProvider />
+            {children}
           </ThemeProvider>
         </ConvexClientProvider>
       </body>
     </html>
-  )
+  );
 }
