b9d4f8112ef34d62c53cb99efd50e5930d0de758
Jun Low <zhijun.low@gmail.com>
2023-10-31 11:38:12 +1100

    update landing page

--- app/(landing)/_components/heading.tsx
+++ app/(landing)/_components/heading.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useConvexAuth } from "convex/react";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, BookText, ScrollText, Goal } from "lucide-react";
 import { SignInButton } from "@clerk/clerk-react";
 import Link from "next/link";
 
@@ -12,11 +12,17 @@ export const Heading = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
 
   return (
-    <div className="max-w-3xl space-y-4">
+    <div className="max-w-5xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your ideas, docs, & projects. Together.
+        Your
+        <ScrollText className="inline w-8 h-8 md:h-12 md:w-12 mx-1 text-amber-400" />
+        <span className="underline">ideas</span>,
+        <BookText className="inline w-8 h-8 md:h-12 md:w-12 mx-1 text-rose-400" />
+        <span className="underline">docs</span>,<br />
+        &<Goal className="inline w-8 h-8 md:h-12 md:w-12 mx-1 text-blue-600" />
+        <span className="underline">projects</span>. Together.
       </h1>
-      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium mt-8">
         Notionize is the connected workspace where <br />
         better, faster work happens.
       </h3>
@@ -42,5 +48,5 @@ export const Heading = () => {
         </SignInButton>
       )}
     </div>
-  )
-}
+  );
+};
